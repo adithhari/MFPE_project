a1ff7e2eefd4b4dae42011c46b0f552d30abc223
Sherin R <sherindaisya@gmail.com>
2022-07-22 11:32:23 +0530

    Updated

---
+++ OrderManagement - Updated/OrderManagement/OrderManagement.API/DTOs/OrderDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderManagement.API.DTOs
+{
+    public class OrderDTO
+    {
+        public long Id { get; set; }
+        [Required]
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        public int TotalAmount { get; set; }
+        
+        
+        public List<ProductDTO> Products { get; set; } = new List<ProductDTO>();
+    }
+}
